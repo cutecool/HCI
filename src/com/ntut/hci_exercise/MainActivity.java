e9c6eb30858b5d055b081173afcde4a42fe3f665
allenhuang <a26732812@gmail.com>
2014-05-21 20:41:28 +0800

    practice4 init commit

--- src/com/ntut/hci_exercise/MainActivity.java
+++ src/com/ntut/hci_exercise/MainActivity.java
@@ -1,12 +1,26 @@
 package com.ntut.hci_exercise;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		FragmentManager manager = getFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		
+//		if(d.getWidth() > d.getHeight()) {
+//			Fragment1 f1 = new Fragment1();
+//			transaction.replace(android.R.id.content, f1);
+//		} else {
+//			Fragment2 f2 = new Fragment2();
+//			transaction.replace(android.R.id.content, f2);
+//		}
+//		transaction.commit();
 	}
 }
