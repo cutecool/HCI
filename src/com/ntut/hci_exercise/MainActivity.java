374af5020ef3bec9dbe4daa81f3f208a91f3255a
cutecool <a26732812@gmail.com>
2014-05-23 01:13:42 +0800

    camera release 會crash.....

--- src/com/ntut/hci_exercise/MainActivity.java
+++ src/com/ntut/hci_exercise/MainActivity.java
@@ -1,12 +1,76 @@
 package com.ntut.hci_exercise;
 
+import android.app.Activity;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceHolder.Callback;
+import android.view.SurfaceView;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+	SurfaceView camaraPreview;
+	SurfaceHolder previewHolder;
+	Camera camera;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		camaraPreview = (SurfaceView)findViewById(R.id.surfaceView1);
+		previewHolder = camaraPreview.getHolder();
+		previewHolder.addCallback(surfaceCallback);
+		previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+	}
+	
+	Callback surfaceCallback = new Callback() {
+		@Override
+		public void surfaceDestroyed(SurfaceHolder holder) {
+			
+		}
+		
+		@Override
+		public void surfaceCreated(SurfaceHolder holder) {
+			camera = Camera.open();
+			try {
+				camera.setPreviewDisplay(previewHolder);
+			} catch (Throwable t) {
+				Log.e("AR Camera", "Exception in setPreviewDisplay()", t);
+				camera.release();
+			}
+		}
+		
+		@Override
+		public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+			Parameters parameters = camera.getParameters();
+			Size size = getBestPreviewSize(width, height, parameters);
+			
+			if(size != null) {
+				parameters.setPreviewSize(size.width, size.height);
+				camera.setParameters(parameters);
+				camera.startPreview();
+			}
+		}
+	};
+	
+	private Size getBestPreviewSize(int width,	int height, Parameters parameters) {
+		Size result = null;
+		for(Size size : parameters.getSupportedPreviewSizes()) {
+			if(size.width <= width && size.height <= height) {
+				if(result == null) {
+					result = size;
+				} else {
+					int resultarea = result.width * result.height;
+					int newarea = size.width * size.height;
+					if(newarea > resultarea) {
+						result = size;
+					}
+				}
+			}
+		}
+		return result;
 	}
 }
