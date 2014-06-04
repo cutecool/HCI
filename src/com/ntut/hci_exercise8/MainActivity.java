dbdb4d068c24770183837e1086c59df4d6a5cf0e
allenhuang <a26732812@gmail.com>
2014-06-05 00:41:16 +0800

    finish square animation

--- src/com/ntut/hci_exercise8/MainActivity.java
+++ src/com/ntut/hci_exercise8/MainActivity.java
@@ -1,64 +1,17 @@
 package com.ntut.hci_exercise8;
 
-import javax.microedition.khronos.opengles.GL10;
-
-import rajawali.RajawaliActivity;
-import rajawali.lights.DirectionalLight;
-import rajawali.materials.Material;
-import rajawali.materials.methods.DiffuseMethod;
-import rajawali.materials.shaders.FragmentShader;
-import rajawali.materials.shaders.VertexShader;
-import rajawali.math.vector.Vector3.Axis;
-import rajawali.primitives.Plane;
-import rajawali.renderer.RajawaliRenderer;
-import android.content.Context;
-import android.graphics.Color;
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends RajawaliActivity {
-	RajawaliRenderer mRenderer;
+public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mRenderer = new BasicRenderer(this);
-		setRenderer(mRenderer);
-		//setContentView(mSurfaceView);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		GLSurfaceView view = new GLSurfaceView(this);
+		view.setRenderer(new SquareRenderer(true));
+		setContentView(view);
 	}
-	
-	private final class BasicRenderer extends RajawaliRenderer {
-		DirectionalLight mLight;
-		Plane mPlane;
-		Material mMaterial;
-		byte maxColor=(byte)255;
-		float colors[] = {
-				maxColor, maxColor, 0, maxColor,
-				0, maxColor, maxColor, maxColor,
-				0, 0, 0, maxColor,
-				maxColor, 0, maxColor, maxColor
-		};
-
-		public BasicRenderer(Context context) {
-			super(context);
-		}
-
-		protected void initScene() {
-			mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
-			mLight.setColor(1.0f, 1.0f, 1.0f);
-			mLight.setPower(2);
-
-			mMaterial = new Material();
-			mMaterial.enableTime(true);
-			
-			mPlane = new Plane(2.0f, 2.0f, 1, 1, Axis.Z, true, true, 1);
-			mPlane.setMaterial(mMaterial);
-			getCurrentScene().addChild(mPlane);
-			getCurrentCamera().setPosition(0, 0, 10);
-		}
-
-		public void onDrawFrame(GL10 glUnused) {
-			super.onDrawFrame(glUnused);
-			mPlane.setRotY(mPlane.getRotY() + 1);
-		}
-	}
-	
 }
