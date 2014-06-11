b3de94880457118b8715c480f20f725d4c968f01
allenhuang <a26732812@gmail.com>
2014-06-11 16:25:54 +0800

    finish practice10

---
+++ src/com/ntut/airhockey/AirHockeyRenderer.java
@@ -0,0 +1,203 @@
+package com.ntut.airhockey;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_FLOAT;
+import static android.opengl.GLES20.GL_LINES;
+import static android.opengl.GLES20.GL_POINTS;
+import static android.opengl.GLES20.GL_TRIANGLES;
+import static android.opengl.GLES20.GL_TRIANGLE_FAN;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+import static android.opengl.GLES20.glDrawArrays;
+import static android.opengl.GLES20.glEnableVertexAttribArray;
+import static android.opengl.GLES20.glGetAttribLocation;
+import static android.opengl.GLES20.glUseProgram;
+import static android.opengl.GLES20.glVertexAttribPointer;
+import static android.opengl.GLES20.glViewport;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView.Renderer;
+
+import com.ntut.airhockey.LoggerConfig;
+import com.ntut.airhockey.ShaderHelper;
+import com.ntut.airhockey.TextResourceReader;
+
+public class AirHockeyRenderer implements Renderer {    
+    private static final String A_POSITION = "a_Position";
+    private static final String A_COLOR = "a_Color";    
+    private static final int POSITION_COMPONENT_COUNT = 2;
+    private static final int COLOR_COMPONENT_COUNT = 3;    
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int STRIDE = 
+        (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    
+    private final FloatBuffer vertexData;
+    private final Context context;
+
+    private int program;
+    private int aPositionLocation;
+    private int aColorLocation;
+
+    public AirHockeyRenderer(Context context) {
+        this.context = context;
+        // Vertex data is stored in the following manner:
+        // The first two numbers are part of the position: X, Y
+        // The next three numbers are part of the color: R, G, B
+        float[] tableVerticesWithTriangles = {   
+            // Order of coordinates: X, Y, R, G, B
+            // Triangle Fan  0 6
+               0f,    0f, 1f, 1f, 1f,         
+            -0.5f, -0.5f, 1f, 1f, 1f,            
+             0.5f, -0.5f, 1f, 1f, 1f,
+             0.5f,  0.5f, 1f, 1f, 1f,
+            -0.5f,  0.5f, 1f, 1f, 1f,
+            -0.5f, -0.5f, 1f, 1f, 1f,
+            
+            //T1  6 3
+               0f, 0.75f, 1f, 1f, 1f,
+            -0.5f,  0.5f, 1f, 1f, 1f,
+             0.5f,  0.5f, 1f, 1f, 1f,
+    
+            //T2  9 3
+            -0.5f, -0.5f , 1f, 1f, 1f,
+               0f, -0.75f, 1f, 1f, 1f,
+             0.5f, -0.5f , 1f, 1f, 1f,
+
+            // Line 1  12 2
+            -0.5f, 0f, 1f, 0f, 0f,
+             0.5f, 0f, 1f, 0f, 0f,
+
+            // L2  14 2
+               0f,  0.5f, 0f, 0f, 1f,
+               0f, -0.5f, 0f, 0f, 1f,
+               
+            // L3  16 2
+            -0.5f, 0.5f, 0f, 0f, 0f,
+             0.5f, 0.5f, 0f, 0f, 0f,
+
+            // L4  18 2
+            -0.5f, -0.5f, 0f, 0f, 0f,
+             0.5f, -0.5f, 0f, 0f, 0f,
+             
+            // P1  20 1
+            -0.25f,  0.25f, 0f, 0f, 0f,
+            // P2  21 1
+             0.25f,  0.25f, 1f, 1f, 0f,
+            // P3  22 1
+             0.25f, -0.25f, 0f, 1f, 1f,
+            // P4  23 1
+             -0.25f, -0.25f, 1f, 0f, 1f,
+        };
+
+        vertexData = ByteBuffer
+            .allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
+            .order(ByteOrder.nativeOrder()).asFloatBuffer();
+
+        vertexData.put(tableVerticesWithTriangles);
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        String vertexShaderSource = TextResourceReader
+            .readTextFileFromResource(context, R.raw.simple_vertex_shader);
+        String fragmentShaderSource = TextResourceReader
+            .readTextFileFromResource(context, R.raw.simple_fragment_shader);
+
+        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int fragmentShader = ShaderHelper
+            .compileFragmentShader(fragmentShaderSource);
+
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
+
+        if (LoggerConfig.ON) {
+            ShaderHelper.validateProgram(program);
+        }
+
+        glUseProgram(program);   
+        
+        aPositionLocation = glGetAttribLocation(program, A_POSITION);
+        aColorLocation = glGetAttribLocation(program, A_COLOR);
+
+        // Bind our data, specified by the variable vertexData, to the vertex
+        // attribute at location A_POSITION_LOCATION.
+        vertexData.position(0);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, 
+            false, STRIDE, vertexData);
+
+        glEnableVertexAttribArray(aPositionLocation);     
+        
+        // Bind our data, specified by the variable vertexData, to the vertex
+        // attribute at location A_COLOR_LOCATION.
+        vertexData.position(POSITION_COMPONENT_COUNT);        
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, 
+            false, STRIDE, vertexData);        
+
+        glEnableVertexAttribArray(aColorLocation);
+    }
+
+    /**
+     * onSurfaceChanged is called whenever the surface has changed. This is
+     * called at least once when the surface is initialized. Keep in mind that
+     * Android normally restarts an Activity on rotation, and in that case, the
+     * renderer will be destroyed and a new one created.
+     * 
+     * @param width
+     *            The new width, in pixels.
+     * @param height
+     *            The new height, in pixels.
+     */
+    @Override
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        // Set the OpenGL viewport to fill the entire surface.
+        glViewport(0, 0, width, height);
+    }
+
+    /**
+     * OnDrawFrame is called whenever a new frame needs to be drawn. Normally,
+     * this is done at the refresh rate of the screen.
+     */
+    @Override
+    public void onDrawFrame(GL10 glUnused) {
+        // Clear the rendering surface.
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw the table.        
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
+        
+        // T1
+        glDrawArrays(GL_TRIANGLES, 6, 3);
+      
+        // T2
+        glDrawArrays(GL_TRIANGLES, 9, 3);
+
+        // L1
+        glDrawArrays(GL_LINES, 12, 2);
+
+        // L2
+        glDrawArrays(GL_LINES, 14, 2);
+        
+        // L3
+        glDrawArrays(GL_LINES, 16, 2);
+        
+        // L4
+        glDrawArrays(GL_LINES, 18, 2);
+        
+        // P1
+        glDrawArrays(GL_POINTS, 20, 1);
+        // P2
+        glDrawArrays(GL_POINTS, 21, 1);
+        // P3
+        glDrawArrays(GL_POINTS, 22, 1);
+        // P4
+        glDrawArrays(GL_POINTS, 23, 1);
+    }
+}
---
+++ src/com/ntut/airhockey/LoggerConfig.java
@@ -0,0 +1,5 @@
+package com.ntut.airhockey;
+
+public class LoggerConfig {
+    public static final boolean ON = true;
+}
